5fb4fae7a70ec1f92f5f374002e3561b5cc9fb3d
ajoao42 <reformado.araujo@gmail.com>
2026-06-10 16:01:51 +0000

    Teste

--- ft_print_int.c
+++ ft_print_int.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_int.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ajoao <ajoao@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/13 16:59:31 by username          #+#    #+#             */
-/*   Updated: 2026/06/09 18:46:28 by ajoao            ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   ft_print_int.c                                    :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: ajoao <ajoao@student.42lisboa.com>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/05/13 16:59:31 by ajoao            #+#    #+#              */
+/*   Updated: 2026/06/10 16:01:00 by ajoao           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,81 +32,77 @@ int	ft_print_int(int nbr)
 	return (i);
 }
 /*
-int	ft_print_int(int nbr)
-{
-	unsigned int	un;
-	int		
 int	main()
 {
-	int i = ft_print_int(-2147483648);
-	printf("\n %3d", i);
-	return (0);
+int i = ft_print_int(-2147483648);
+printf("\n %3d", i);
+return (0);
 }
 
 #include "ft_printf.h"
 
 int	ft_print_int(int nbr)
 {
-	unsigned int	un;
-	int				i;
-	i = 0;
-	if (nbr < 0)
-	{
-		i += 1; //ft_print_chr('-');
-		un = (unsigned int)(-(long) nbr);
-	}
-	else
-		un = (unsigned int) nbr;
-	if (un >= 10)
-		i += (ft_print_int(un / 10) + 1);
-	else 
-		i += 1; //ft_print_chr((un % 10) + '0');
-	return (i);
+unsigned int	un;
+int				i;
+i = 0;
+if (nbr < 0)
+{
+i += 1; //ft_print_chr('-');
+un = (unsigned int)(-(long) nbr);
+}
+else
+un = (unsigned int) nbr;
+if (un >= 10)
+i += (ft_print_int(un / 10) + 1);
+else
+i += 1; //ft_print_chr((un % 10) + '0');
+return (i);
 }
 */
 /*
 int	main()
 {
-	int i = ft_print_int(-2147483648);
-	printf("\n %3d", i);
-	return (0);
+int i = ft_print_int(-2147483648);
+printf("\n %3d", i);
+return (0);
 }
 */
 
 /*
 void ft_print_nbr(int nbr)
 {
-	unsigned int un;
+unsigned int un;
 
-	if (nbr < 0)
-	{
-		ft_print_chr('-');
-		un = (unsigned int)(-(long)nbr);
-	}
-	else
-		un = (unsigned int)nbr;
+if (nbr < 0)
+{
+ft_print_chr('-');
+un = (unsigned int)(-(long)nbr);
+}
+else
+un = (unsigned int)nbr;
 
-	if (un >= 10)
-		ft_print_nbr(un / 10);
-	ft_print_chr((un % 10) + '0');
+if (un >= 10)
+ft_print_nbr(un / 10);
+ft_print_chr((un % 10) + '0');
 }
 */
 
 /*
 void ft_print_nbr(int nbr)
 {
-	unsigned int un;
+unsigned int un;
 
-	if (nbr < 0)
-	{
-		ft_print_chr('-');
-		un = (unsigned int)(-(long)nbr);
-	}
-	else
-		un = (unsigned int)nbr;
+if (nbr < 0)
+{
+ft_print_chr('-');
+un = (unsigned int)(-(long)nbr);
+}
+else
+un = (unsigned int)nbr;
 
-	if (un >= 10)
-		ft_print_nbr(un / 10);
-	ft_print_chr((un % 10) + '0');
+if (un >= 10)
+ft_print_nbr(un / 10);
+ft_print_chr((un % 10) + '0');
 }
 */
